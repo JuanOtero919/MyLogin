5725203c9662ff409fd44006fca46934ad90669d
JuanOtero919 <juanoterov@unicauca.edu.co>
2023-06-14 11:27:15 -0500

    Screen Vacias

---
+++ app/src/main/java/com/example/mylogin/screensDashboard/P_Historial.kt
@@ -0,0 +1,12 @@
+package com.example.mylogin.screensDashboard
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ScreenHistorial(){
+    Column() {
+        Text(text = "Pagina Historial")
+    }
+}
