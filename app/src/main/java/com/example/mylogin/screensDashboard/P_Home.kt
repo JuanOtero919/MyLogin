768d952bd4ba3ef1b20b492d0fc845ce5df1f771
JuanOtero919 <juanoterov@unicauca.edu.co>
2023-06-15 02:35:53 -0500

    Food Card

--- app/src/main/java/com/example/mylogin/screensDashboard/P_Home.kt
+++ app/src/main/java/com/example/mylogin/screensDashboard/P_Home.kt
@@ -1,12 +1,58 @@
 package com.example.mylogin.screensDashboard
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mylogin.R
+import com.example.mylogin.recursos.FoodCard
 
 @Composable
 fun ScreenHome(){
-    Column() {
-        Text(text = "Pagina Home")
+    val cardTitle = "Pizza Hawaiana"
+    val cardDescription = "Deliciosa pizza con jamón, piña y salsa de tomate"
+    val cardImage: Painter = painterResource(R.drawable.imagenproducto)
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(vertical = 16.dp)
+    ) {
+        item {
+            FoodCard(
+                title = cardTitle,
+                description = cardDescription,
+                image = cardImage,
+                onBuyClick = { /* Acción al hacer clic en el botón Comprar */ }
+            )
+        }
+        item {
+            FoodCard(
+                title = cardTitle,
+                description = cardDescription,
+                image = cardImage,
+                onBuyClick = { /* Acción al hacer clic en el botón Comprar */ }
+            )
+        }
+        item {
+            FoodCard(
+                title = cardTitle,
+                description = cardDescription,
+                image = cardImage,
+                onBuyClick = { /* Acción al hacer clic en el botón Comprar */ }
+            )
+        }
     }
+}
+
+@Composable
+@Preview
+fun prevHome(){
+    ScreenHome()
 }
