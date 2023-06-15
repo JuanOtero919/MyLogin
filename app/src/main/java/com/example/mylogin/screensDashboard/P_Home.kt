635432f36f444ec63f872867aeb2cd48fa858ef7
JuanOtero919 <juanoterov@unicauca.edu.co>
2023-06-15 03:02:39 -0500

    Tres tipos de productos para las cards

--- app/src/main/java/com/example/mylogin/screensDashboard/P_Home.kt
+++ app/src/main/java/com/example/mylogin/screensDashboard/P_Home.kt
@@ -1,58 +1,55 @@
 package com.example.mylogin.screensDashboard
 
-import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
+
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.mylogin.R
 import com.example.mylogin.recursos.FoodCard
+import com.example.mylogin.recursos.FoodProduct
 
 @Composable
-fun ScreenHome(){
-    val cardTitle = "Pizza Hawaiana"
-    val cardDescription = "Deliciosa pizza con jamón, piña y salsa de tomate"
-    val cardImage: Painter = painterResource(R.drawable.imagenproducto)
-
+fun ScreenHome() {
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(vertical = 16.dp)
     ) {
         item {
             FoodCard(
-                title = cardTitle,
-                description = cardDescription,
-                image = cardImage,
+                title = FoodProduct.Cafe.name,
+                description = FoodProduct.Cafe.description,
+                image = painterResource(FoodProduct.Cafe.image),
                 onBuyClick = { /* Acción al hacer clic en el botón Comprar */ }
             )
         }
         item {
             FoodCard(
-                title = cardTitle,
-                description = cardDescription,
-                image = cardImage,
+                title = FoodProduct.Pan.name,
+                description = FoodProduct.Pan.description,
+                image = painterResource(FoodProduct.Pan.image),
                 onBuyClick = { /* Acción al hacer clic en el botón Comprar */ }
             )
         }
         item {
             FoodCard(
-                title = cardTitle,
-                description = cardDescription,
-                image = cardImage,
+                title = FoodProduct.Pastel.name,
+                description = FoodProduct.Pastel.description,
+                image = painterResource(FoodProduct.Pastel.image),
                 onBuyClick = { /* Acción al hacer clic en el botón Comprar */ }
             )
         }
     }
 }
 
+
 @Composable
 @Preview
-fun prevHome(){
+fun PrevHome(){
     ScreenHome()
 }
